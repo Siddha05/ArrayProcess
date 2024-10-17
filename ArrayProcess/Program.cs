dc493d6c6fa450288b741c7b024664fffd9fe938
Богатов <siddha05.sb@gmail.com>
2024-10-17 12:56:29 +0300

    Add project files.

---
+++ ArrayProcess/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ArrayProcess
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        public (int min, int max, double[] avgs) Process(int[,] array)
+        {
+            int min = array[0,0];
+            int max = array[0,0];
+            double[] avg = new double[array.GetLength(0)];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                double row_avg = default;
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i,j] < min) min = array[i,j];
+                    if (array[i,j] > max) max = array[i,j];
+                    row_avg += array[i,j];
+                }
+                avg[i] = row_avg / array.GetLength(1);
+            }
+            return (min, max, avg);
+        }
+    }
+}
---
+++ ArrayProcessTests/ProgramTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArrayProcess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProcess.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        private static IEnumerable<object[]> ArraySource()
+        {
+            yield return new object[]
+            {
+                new int[,]{ { 1, 2 }, { 3, 4 } },
+                1,
+                4,
+                new double[] { 1.5, 3.5 }
+            };
+
+            yield return new object[]
+            {
+                new int[,]{ { 1, 2 }, { 3, 4 }, { 13, 1} },
+                1,
+                13,
+                new double[] { 1.5, 3.5, 7d }
+            };
+            yield return new object[]
+            {
+                new int[,]{ { 10, 2 }, { -3, 4 }, { 13, 11} },
+                -3,
+                13,
+                new double[] { 6, 0.5, 12d }
+            };
+        }
+        
+        [TestMethod()]
+        [DynamicData(nameof(ArraySource), DynamicDataSourceType.Method)]
+        public void ProcessTest(int[,] data, int exp_min, int exp_max, double[] exp_avg)
+        {
+            var res = new Program().Process(data);
+
+            Assert.AreEqual(exp_min, res.min);
+            Assert.AreEqual(exp_max, res.max);
+            CollectionAssert.AreEqual(exp_avg, res.avgs);
+        }
+    }
+}
